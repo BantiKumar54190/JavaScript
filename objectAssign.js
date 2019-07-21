cc4ef1d56c1a0ed5fa4f07248e3a84b8abbade5c
bantikumar5642 <banti.kumar5642@outlook.com>
2019-07-21 16:41:59 +0530

    object Assign

---
+++ objectAssign.js
@@ -0,0 +1,24 @@
+var engine = {
+
+    gear: function() {
+        return 'gear'
+    },
+    drive: function() {
+        return 'driver'
+    } 
+ 
+};
+
+var toyotaEngine = {
+    gear: function(){
+        return 6;
+    },
+    update: function()
+    {
+        return "updated engine" 
+    }
+};
+
+var combinedObject =  Object.assign(toyotaEngine, engine);
+console.log(combinedObject)
+console.log(combinedObject.gear());
