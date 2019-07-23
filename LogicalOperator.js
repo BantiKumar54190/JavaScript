01210abeb6f1669958e292131a4a14387989b0d0
bantikumar5642 <banti.kumar5642@outlook.com>
2019-07-23 18:37:01 +0530

    Logical And && OR Operator

---
+++ LogicalOperator.js
@@ -0,0 +1,14 @@
+ function t() {
+    return true;
+}
+
+ function u() {
+    return false;
+}
+console.log(t)
+p = (t && u);
+console.log(p)
+var x = true ;
+var y = false;
+console.log(x && y);
+console.log(x||y )
