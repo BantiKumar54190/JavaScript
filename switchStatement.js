0760d9fbe64c77652a69a79337ebc8874b2cba34
bantikumar5642 <banti.kumar5642@outlook.com>
2019-07-25 10:15:55 +0530

    switch statement in javascript

---
+++ switchStatement.js
@@ -0,0 +1,11 @@
+var name = "na";
+switch(name) {
+  case 'na' :
+       console.log("not available") ;
+       break;
+  case 'banti':
+      console.log("Banti is available");
+       break;
+  default:
+      console.log("Nothing Available");          
+}
