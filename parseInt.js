d2503045c390102e0482be4b8c6f989920c8778b
bantikumar5642 <banti.kumar5642@outlook.com>
2019-07-25 09:29:14 +0530

    parse int basics

---
+++ parseInt.js
@@ -0,0 +1,10 @@
+function bramha(number , base) {
+    var parse = parseInt(number,base);
+    if(isNaN(parse))
+    {
+      return 0;
+    }
+    return parse;
+}
+
+console.log(bramha(+Infinity, 8))
