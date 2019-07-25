53b68adddfabf5cfa5f2d1678653e1cb7a5cdc01
bantikumar5642 <banti.kumar5642@outlook.com>
2019-07-25 22:04:09 +0530

    hoisting javascript basics

--- hoisting.js
+++ hoisting.js
@@ -1,8 +1,23 @@
+"use strict "
 var hoisting = function() {
-     x = 5;
-    var y ;
+    var    y
+    x = 5;
+  
     console.log(x,y);//This is the case where x is notdeclared but defined but in case of   
-    var x;
+   
 }
-hoisting();
+//hoisting();
+
 console.log(x)
+
+
+var y = 2 ;
+var x = function() {
+    
+    console.log(y);
+    var y;//This is called hoisting in javaScript as y gets declared at the top so it is undefined in this case 
+
+
+}
+x();
+
