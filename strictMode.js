01dc7cc747b6dc3a3cb7a492558ac6b84b54b8e5
bantikumar5642 <banti.kumar5642@outlook.com>
2019-07-25 23:31:43 +0530

    use of strict mode for the first time

---
+++ strictMode.js
@@ -0,0 +1,12 @@
+// "use strict";
+
+var hoisting = function() {
+
+    var y;
+    x = 5;
+  
+    console.log(x,y);//This is the case where x is notdeclared but defined but in case of
+}
+//hoisting();
+console.log("11",  x);
+
