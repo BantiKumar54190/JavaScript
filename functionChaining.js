71c1e40b3002d3faf45a62898bcd965cdf7215c0
bantikumar5642 <banti.kumar5642@outlook.com>
2019-07-26 21:44:47 +0530

    function chaining basics

---
+++ functionChaining.js
@@ -0,0 +1,21 @@
+const obj = function(i) {
+    this.i = i;
+    this.add = function(i) {
+        this.i += i;
+        return this;
+    };
+    this.subtract = function(i) {
+       this.i -= i; 
+       return this;
+    };
+    this.print = function() {
+        console.log(this.i);
+        return this;
+    }
+}; 
+
+const x = new obj(6);  
+x.subtract(2).add(3).print();
+
+const banti = new obj(6);  
+banti.print();
