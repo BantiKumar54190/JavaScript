eb994401311409e2875d2bc0d10903536d912fe7
bantikumar5642 <banti.kumar5642@outlook.com>
2019-08-29 22:07:30 +0530

    Currying functions behavior

---
+++ functionCurring.js
@@ -0,0 +1,13 @@
+var  volume = function(length) {
+          debugger;
+      return function  surfaceArea (breadth) {
+              debugger;
+          return function height  (height) {
+                  debugger;
+              return height*breadth*length;
+                
+          }
+      }
+}
+
+volume(2)(3)(4);
